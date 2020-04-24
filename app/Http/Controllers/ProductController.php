83b60f611ae67339d0142060d20cdf66d0b50850
Jeferson Costa <rc.jeferson@gmail.com>
2020-04-23 21:19:53 -0300

    feat(Product): Adiciona controller e rota
    Adiciona estrutura base do controller e cria rota apontando para esse
    controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+class ProductController extends Controller
+{
+    protected $request;
+    protected $product;
+
+    public function __construct(Request $request, Product $product)
+    {
+        $this->request = $request;
+        $this->product = $product;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreUpdateProductRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUpdateProductRequest $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreUpdateProductRequest  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreUpdateProductRequest $request, $id)
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+    }
+
+    /**
+     * Search Products
+     */
+    public function search(Request $request)
+    {
+    }
+}
