94db99892e15dcf14af5890078d204f6424e9ed8
Jeferson Costa <rc.jeferson@gmail.com>
2020-05-03 18:07:13 -0300

    feat: Adiciona autenticação
    Adiciona estrutura necessária para habilitar autenticação no Laravel.

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::any('products/search', 'ProductController@search')->name('products.search');
-Route::resource('products', 'ProductController');
+Route::any('products/search', 'ProductController@search')->name('products.search')->middleware('auth');
+Route::resource('products', 'ProductController')->middleware(['auth', 'check.is.admin']);
 
 Route::get('/', function () {
     return redirect()->route('products.index');
 });
+
+Auth::routes(['register' => false]);
